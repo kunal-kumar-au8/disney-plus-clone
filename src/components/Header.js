6b8e56f88b1ae65cbc5a3adf132ec6b442956202
Kunal Kumar <kunalkseven@gmail.com>
2021-06-28 11:50:56 +0530

    intial header done

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,101 @@
-import React from 'react'
+import React from 'react';
+import styled from 'styled-components';
+
+const Nav = styled.nav`
+  height: 70px;
+  background: #090b13;
+  display: flex;
+  align-items: center;
+  padding: 0 36px;
+`;
+const Logo = styled.img`
+  width: 80px;
+`;
+const NavMenu = styled.div`
+  display: flex;
+  flex: 1;
+  margin-left: 25px;
+  align-items: center;
+  a {
+    display: flex;
+    align-items: center;
+    padding: 0 12px;
+    cursor: pointer;
+
+    img {
+      height: 20px;
+    }
+
+    span {
+      font-size: 13px;
+      letter-spacing: 1.42px;
+      position: relative;
+
+      &:after{
+          content:"";
+          height:2px;
+          background: white;
+          position: absolute;
+          left:0;
+          right:0;
+          bottom:-6px;
+          opacity: 0;
+          transform-origin: left center;
+          transition: all 250ms cubic-bezier(0.25, 0.46, 0.45, 0.94) 0s;
+          transform: scaleX(0);
+      }
+    }
+    &:hover{
+        span:after{
+            transform: scaleX(1);
+            opacity: 1;
+        }
+    }
+  }
+`;
+const UserImg = styled.img`
+  width: 40px;
+  height: 40px;
+  border-radius: 50px;
+  cursor: pointer;
+`;
 
 const Header = () => {
-    return (
-        <div>
-            
-        </div>
-    )
-}
+  return (
+    <Nav>
+      <Logo src='/images/logo.svg' />
+      <NavMenu>
+        <a>
+          <img src='/images/home-icon.svg' />
+          <span>HOME</span>
+        </a>
+        <a>
+          <img src='/images/search-icon.svg' />
+          <span>SEARCH</span>
+        </a>
+        <a>
+          <img src='/images/watchlist-icon.svg' />
+          <span>WATCHLIST</span>
+        </a>
+        <a>
+          <img src='/images/original-icon.svg' />
+          <span>ORIGiNAL</span>
+        </a>
+        <a>
+          <img src='/images/movie-icon.svg' />
+          <span>MOVIES</span>
+        </a>
+        <a>
+          <img src='/images/series-icon.svg' />
+          <span>SERIES</span>
+        </a>
+      </NavMenu>
+      <UserImg
+        src='https://media-exp3.licdn.com/dms/image/D5635AQEETadYBcSNbQ/profile-framedphoto-shrink_100_100/0/1623302260826?e=1624946400&v=beta&t=X8suPnm9Zh1D5hpr8_GHd1Mlg4hdeaSGEW5lxSFF_eI'
+        alt='user'
+      />
+    </Nav>
+  );
+};
 
-export default Header
+export default Header;
